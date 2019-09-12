0bc92cee0445c745ddc7e275f29b401f79d308a6
raythurman2386 <raymondthurman5@gmail.com>
2019-09-11 21:40:56 -0400

    almost complete

--- src/components/AnimalForm.js
+++ src/components/AnimalForm.js
@@ -1,28 +1,70 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { withFormik, Form, Field } from 'formik'
+import * as Yup from 'yup'
+import Axios from 'axios'
 
-const AnimalForm = (props) => {
-  const [animal, setAnimal] = useState({
-    species: ''
-  })
-
-  const handleChange = event => {
-    setAnimal({
-      ...animal,
-      [event.target.name]: event.target.value
-    })
-  }
-
-  const handleSubmit = event => {
-    event.preventDefault()
-    console.log(animal)
-  }
-
+const AnimalForm = ({ errors, touched, status }) => {
+  console.log(status)
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="species" placeholder="Species" onChange={handleChange} />
-      <button type="submit">Submit</button>
-    </form>
+    <Form>
+      {touched.species && errors.species && (
+        <p className='error'>{errors.species}</p>
+      )}
+      <Field type='text' name='species' placeholder='Species' />
+      {touched.age && errors.age && <p className='error'> {errors.age}</p>}
+      <Field type='number' name='age' placeholder='Age' />
+      {touched.diet && errors.diet && <p className='error'> {errors.diet}</p>}
+      <Field component='select' name='diet'>
+        <option value='' disabled>
+          Select Diet
+        </option>
+        <option value='meat'>Meat</option>
+        <option value='veggies'>Veggies</option>
+        <option value='both'>Both</option>
+      </Field>
+      {touched.vaccinations && errors.vaccinations && (
+        <p className='error'> {errors.vaccinations}</p>
+      )}
+      <label>
+        <Field type='checkbox' name='vaccinations' />
+        <span>Vaccinated?</span>
+      </label>
+      <Field component='textarea' name='notes' placeholder='Notes' />
+      <button type='submit'>Submit</button>
+      Species: {status.species}
+      <br />
+      Age: {status.age}
+    </Form>
   )
 }
 
-export default AnimalForm
+export default withFormik({
+  mapPropsToValues: ({ species, age, diet, vaccinations, notes }) => {
+    return {
+      species: species || '',
+      age: age || '',
+      diet: diet || '',
+      vaccinations: vaccinations || false,
+      notes: notes || '',
+    }
+  },
+
+  // Validation
+  validationSchema: Yup.object().shape({
+    species: Yup.string().required('Species is required'),
+    age: Yup.number()
+      .positive()
+      .required('Age is required'),
+    diet: Yup.string().required('Diet is required'),
+    vaccinations: Yup.boolean().oneOf([true]),
+  }),
+
+  handleSubmit(values, { setStatus }) {
+    Axios.post('https://reqres.in/api/animals', values)
+      .then(res => {
+        console.log(res)
+        setStatus(res.data)
+      })
+      .catch(err => console.log(err))
+  },
+})(AnimalForm)
